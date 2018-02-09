fbfe8d8cedb6f0c15d0a045466c58295a6167690
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2018-02-09 19:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const app = "I don't do much."
 
 function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
   return kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function des
